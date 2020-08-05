e9a803b64bbd7196b4c71159425387334f9aca66
Geo <Geo@DESKTOP-4HDFJN5>
2020-08-05 18:17:00 +0300

    Add project files.

---
+++ LoginSystem/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index() // Creeam un View pentru Homecontroller, click stanga pe Index() si  AddView
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginSystem/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginSystem.Models;
+
+namespace LoginSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index() // pagina web
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(LoginSystem.Models.User userModel) 
+        { 
+            using (LoginDataBaseEntities db = new LoginDataBaseEntities()) {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["UserName"] = userDetails.UserName;
+                    return RedirectToAction("Index","Home");   // localhos/Home/Index  !!!! Trebuie sa creeam HomeControler ca sa functioneze!
+                }
+            }
+       
+        }
+        public ActionResult LogOutFunction()
+        {
+            int UserId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+
+        }
+    }
+}
